25ce2388e7022bff549798c8df365f397e5cdd63
michaelngosa <ngosamichael@gmail.com>
2022-06-02 01:27:36 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * this program prints "Programming is like building a multilingual puzzle
+ *
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
