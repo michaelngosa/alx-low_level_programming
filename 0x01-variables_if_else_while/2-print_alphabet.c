754ae87dc9495aa76e2019717ad39c930f161919
michaelngosa <ngosamichael@gmail.com>
2022-06-03 07:27:43 -0700

    the alphabet game

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
-/*
- * main - print if the number is postive, zero, or negative
- * 
+/**
+ * main - print if the number is postive, zero, or negative 
  * Description: using the main function
  * this program prints "the alphabet in lowercase
  * Return: alway returns 0
