868abd9f552eb807fad9ee55c7a33229b37f35a7
michaelngosa <ngosamichael@gmail.com>
2022-06-03 03:20:39 -0700

    C program that prints

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry point
- *
- * return: always 0
+ * main - entry
+ * return: always 1 (success)
  */
 int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return(0);
+return(1);
 }
