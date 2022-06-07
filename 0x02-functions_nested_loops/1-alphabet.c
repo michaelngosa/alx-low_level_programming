32a32ec6ccd4a5514aac8d5f90a293b8df0667bf
michaelngosa <ngosamichael@gmail.com>
2022-06-07 16:21:09 -0700

    prints the alphabet, in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,11 +9,11 @@
  */
 void print_alphabet(void)
 {
-char i;
-for (i = 'a'; i <= 'z'; i++)
+char alphabet;
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 {
-_putchar(i);
+_putchar(alphabet);
 }
 _putchar('\n');
-return (0);
+
 }
