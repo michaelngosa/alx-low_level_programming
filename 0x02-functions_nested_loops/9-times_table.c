41851a0c66b6ac0a93e0e1cbbb7f37777208ec08
michaelngosa <ngosamichael@gmail.com>
2022-06-07 18:50:54 -0700

    prints the 9 times table, starting with 0

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,30 +16,30 @@ if ((k / 10) == 0)
 {
 if (j == 0)
 {
-_putchar ('0');
+_putchar('0');
 }
 if (j != 0)
 {
-_putchar (' ');
-_putchar (( k % 10) + '0');
+_putchar(' ');
+_putchar(( k % 10) + '0');
 }
 if (j < 9)
 {
-_putchar (',');
-_putchar (' ');
+_putchar(',');
+_putchar(' ');
 }
 }
 else
 {
-_putchar ((k / 10) + '0');
-_putchar ((k % 10) + '0');
+_putchar((k / 10) + '0');
+_putchar((k % 10) + '0');
 if (j < 9)
 {
-_putchar (',');
-_putchar (' ');
+_putchar(',');
+_putchar(' ');
 }
 }
 }
-_putchar ('\n');
+_putchar('\n');
 }
 }
