309bb22c17cd5a443a43d8dd4e1272f2c7738b62
michaelngosa <ngosamichael@gmail.com>
2022-06-25 20:24:18 -0700

    script called create_static_lib.sh that creates a static library

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _memset - This function fills memory with a constant byte
  * @s: is pointer to a char
@@ -7,12 +6,11 @@
  * @n: is an unsigned int
  * Return: Returns the transformed pointer
  */
-
 char *_memset(char *s, char b, unsigned int n)
 {
 unsigned int i;
 
 	for (i = 0; i < n; i++)
-		*s[i] = b;
+		*(s + i) =  b;
 	return (s);
 }
