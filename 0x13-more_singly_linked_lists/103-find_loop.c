ed315eea76e44e7f85b39126e5055ed365214c3b
michaelngosa <ngosamichael@gmail.com>
2022-07-19 15:14:00 -0700

    Function to find loop

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -2,40 +2,31 @@
 
 /**
  * find_listint_loop - finds the loop in a linked list.
- * @head: head of a list.
+ * @head: pointer to the beginning of the list
  *
- * Return: the address of the node where the loop starts.
+ * Return: address of the node where the loop starts or NULL if there's no loop
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *p2;
-	listint_t *prev;
+listint_t *tortoise, *hare;
 
-	p2 = head;
-	while (head && p2 && p2->next)
-	{
-		head = head->next;
-		p2 = p2->next->next;
-
-		if (head == p2)
-		{
-			head = prev;
-			prev = p2;
-			while (1)
-			{
-				p2 = prev;
-				while (p2->next != head && p2->next != prev)
-				{
-					p2 = p2->next;
-				}
-				if (p2->next == head)
-					break;
-
-				head = head->next;
-			}
-			return (p2->next);
-		}
-	}
-
-	return (NULL);
+tortoise = hare = head;
+while (tortoise && hare && hare->next)
+{
+tortoise = tortoise->next;
+hare = hare->next->next;
+if (tortoise == hare)
+{
+tortoise = head;
+break;
+}
+}
+if (!tortoise || !hare || !hare->next)
+return (NULL);
+while (tortoise != hare)
+{
+tortoise = tortoise->next;
+hare = hare->next;
+}
+return (hare);
 }
